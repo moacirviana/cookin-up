a97b126f543c058f6988784c6342bad7df489dc4
moacirviana <moacir.viana@gmail.com>
2025-02-19 12:01:18 -0400

    Initial commit

---
+++ src/components/TemplateCompo.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Template</h1>
+   
+   </template>
+   
+   <script lang="ts">
+   export default {
+     name: 'TemplateCompo'
+   }
+   </script>
+   
+   <style scoped>
+   
+   
+   </style>
+   
